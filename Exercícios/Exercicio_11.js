18aa7dfc1e504a40501c83b15edb6250adbf662e
Thiago Marchini <thiago@msn.com>
2021-02-24 19:29:06 -0300

    Alteraçoes no Exercicio 11

--- Exercícios/Exercicio_11.js
+++ Exercícios/Exercicio_11.js
@@ -1,40 +1,40 @@
-let salarioBruto = 3000;
+let salarioBruto = 3800.00;
 let descontoINSS;
 let descontoIR;
 
 //Desconto do INSS
 switch (true) {
-  case (salarioBruto <= 1556.94):
-    descontoINSS = salarioBruto * 0.08;
-    break;
-  case (1556.95 <= salarioBruto && salarioBruto <= 2594.92):
-    descontoINSS = salarioBruto * 0.09;
+  case (salarioBruto >= 5189.83):
+    descontoINSS = 570.88;
     break;
   case (2594.93 <= salarioBruto && salarioBruto <= 5189.82):
     descontoINSS = salarioBruto * 0.11;
     break;
+  case (1556.95 <= salarioBruto && salarioBruto <= 2594.92):
+    descontoINSS = salarioBruto * 0.09;
+    break;
   default:
-    descontoINSS = 570.88;
+    descontoINSS = 0;
 }
 
 let salarioBase = salarioBruto - descontoINSS;
 
 //desconto IR
 switch (true) {
-  case (salarioBase <= 1903.98):
-    descontoIR = 0;
+  case (salarioBase >= 4664.69):
+    descontoIR = salarioBase * 0.275 - 869.36;
     break;
-  case (1903.99 <= salarioBase && salarioBase <= 2826.65):
-    descontoIR = salarioBase * 0.075 - 142.80;
+  case (3751.06 <= salarioBase && salarioBase <= 4664.68):
+    descontoIR = salarioBase * 0.225 - 636.13;
     break;
   case (2826.66 <= salarioBase && salarioBase <= 3751.05):
     descontoIR = salarioBase * 0.15 - 354.80;
     break;
-  case (3751.06 <= salarioBase && salarioBase <= 4664.68):
-    descontoIR = salarioBase * 22.5 - 636.13;
+  case (1903.99 <= salarioBase && salarioBase <= 2826.65):
+    descontoIR = salarioBase * 0.075 - 142.80;
     break;
   default:
-    descontoIR = salarioBase * 27.5 - 869.36;
+    descontoIR = 0;
 }
 
 let salarioLiquido = salarioBase - descontoIR;
