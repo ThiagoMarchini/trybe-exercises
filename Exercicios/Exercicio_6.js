c13c1dd2a552e74bf31bd8f25a40cdf031707b35
Thiago Marchini <thiago@msn.com>
2021-02-25 15:23:06 -0300

    adicao da resolucao dos exercicios 5 a 9

---
+++ Exercicios/Exercicio_6.js
@@ -0,0 +1,14 @@
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let totalDeImpares = 0;
+for(let index = 0; index < numbers.length; index++){
+  if(numbers[index] % 2 != 0){
+    totalDeImpares++;
+  }
+}
+if(totalDeImpares != 0){
+  console.log("total de ímpares encontrado: "+totalDeImpares)
+}
+else {
+  console.log("nenhum ímpar encontrado");
+}
---
+++ Exercicios/Exercicio_9.js
@@ -0,0 +1,9 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+let array = [];
+
+for(let index = 0; index <= 25; index++){
+  array.push(index);
+}
+for(index = 0; index < array.length; index++){
+  console.log(array[index]/2);
+}
