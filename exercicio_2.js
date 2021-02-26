ffd89a394f98dc953004e22198bb7ba5fe84efde
Thiago Marchini <thiago@msn.com>
2021-02-25 22:47:52 -0300

    adiciona exercicio 2 resolvido

---
+++ exercicio_2.js
@@ -0,0 +1,8 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+let n = 5;
+let b = '';
+
+for (let index = 1; index <= n; index += 1){
+  b+='*'
+  console.log(b);
+}
