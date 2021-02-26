cb201c7b0899417ccc470037c4600e5d8aa67c79
Thiago Marchini <thiago@msn.com>
2021-02-25 23:02:17 -0300

    adiciona o exercicio 3 ainda por resolver

--- exercicio_3.js
+++ exercicio_3.js
@@ -1,1 +1,9 @@
-//3- Agora inverta o lado do triângulo. Por exemplo:
+//3- Agora inverta o lado do triângulo. Por exemplo
+
+let n = 5;
+let b = '';
+
+for (let index = n; index < 0; index -= 1){
+  b+='*';
+  console.log(b);
+}
