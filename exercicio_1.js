993ffd47f517a6d6da7e2ffc6d6d70a0d73569d7
Thiago Marchini <thiago@msn.com>
2021-02-26 15:55:36 -0300

    modificado codigo do ex. 1

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,12 +1,9 @@
 let n = 5;
-let b = '*';
+let b = '';
 
-for (let index = 1; index < n; index += 1){
+for (let index = 0; index < n; index += 1){
   b+='*'
 }
 for(let index = 0; index < n; index += 1){
-  
   console.log(b);
 }
-
-//console.log(b)
