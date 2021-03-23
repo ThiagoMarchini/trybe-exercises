427511f88ee4410c5bac03cfe3c149349aa5543e
Thiago Marchini <thiago@msn.com>
2021-03-23 17:59:22 -0300

    exercicios do conteudo

---
+++ var-let-const/parte1-1.js
@@ -0,0 +1,9 @@
+if (true) {
+  // inicio do escopo do if
+  var userAge = "20";
+  // const userAge = "20";
+  // let userAge = "20";
+  console.log(userAge); // 20
+  // fim do escopo do if
+}
+console.log(userAge); // 20
