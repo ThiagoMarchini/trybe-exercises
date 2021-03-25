b73eb51f01ef214cb9fffdddab039dbacd025c13
Thiago Marchini <thiago@msn.com>
2021-03-25 18:55:36 -0300

    todos exercĩcios completos

--- exercises/Bonus/Bonus-5-kata.js
+++ exercises/Bonus/Bonus-5-kata.js
@@ -7,30 +7,27 @@ function verify(entry) {
   else if (entry.length < 8) {throw new Error('password must be at least 8 characters long')}
   else if (!entry.match(/[a-z]/)) {throw new Error('password must have at least 1 lowercase character')}
   else if (!entry.match(/[A-Z]/)) {throw new Error('password must have at least 1 uppercase character')}
-  else if (!entry.match(/[0-9]/)) {throw new Error('password must have at least 1 number')}
-  else { return console.log('password OK!')};
+  else if (!/[0-9]/.test(entry)) {throw new Error('password must have at least 1 number')}
+  else {return console.log('password OK!')};
 }
 
 // password should be larger than 8 chars
 let password = '1234567';
-// console.log(verify(password));
 assert.throws(() => { verify(password); }, /^Error: password must be at least 8 characters long$/);
 
 // password should not be null
 assert.throws(() => { verify(); }, /^Error: password must not be null$/);
 
-// password should have one uppercase letter at least
+// // password should have one uppercase letter at least
 password = 'ASDFQWER';
 assert.throws(() => { verify(password); }, /^Error: password must have at least 1 lowercase character$/);
 
-// password should have one lowercase letter at least
+// // password should have one lowercase letter at least
 password = 'asdfqwer';
 assert.throws(() => { verify(password); }, /^Error: password must have at least 1 uppercase character$/);
 
 // password should have one number at least
-password = 'asdaASDA';
+password = 'asdfQWER';
 assert.throws(() => { verify(password); }, /^Error: password must have at least 1 number$/);
-
-//verify('asdaASDA');
 
 // Each one of these should throw an exception with a different message of your choosing
