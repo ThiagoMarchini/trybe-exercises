b6c7409737766ec4b44852dc555aef5ccfc59ac9
Thiago Marchini <thiago@msn.com>
2021-03-28 12:04:40 -0300

    completed exercises 1 and 2

---
+++ exercicios/exercicio-2.js
@@ -0,0 +1,15 @@
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+const check = (num1, num2) => {
+  return (num1 === num2);
+}
+
+const resultado = (number, action) => {
+  const pulled = Math.ceil(Math.random() * 5);
+  console.log(pulled);
+  if (action(number, pulled)) {
+    console.log('Parabéns você ganhou');
+  } else {
+    console.log('Tente novamente');
+  }
+}
+resultado(3, check);
