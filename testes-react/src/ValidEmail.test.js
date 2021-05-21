fbb928c3baddc1818fb1b8cb941ced359fc2dbcc
Thiago Marchini <thiago@msn.com>
2021-05-21 16:22:46 -0300

    feito

--- testes-react/src/ValidEmail.test.js
+++ testes-react/src/ValidEmail.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import ValidEmail from './ValidEmail';
 
-test('Testando um componente, caso o email seja valido.', () => {
-  const EMAIL_USER = 'email@email.com';
-  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
-  const isValid = getByText('Email Valido');
-  expect(isValid).toBeInTheDocument();
-});
+describe('Testando entrada de emails', () => {
+  it('Testando um componente, caso o email seja valido.', () => {
+    const EMAIL_USER = 'email@email.com';
+    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const isValid = getByText('Email Valido');
+    expect(isValid).toBeInTheDocument();
+  })
+
+  it('Testando um componente, caso o email seja inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const isValid = getByText('Email Inválido');
+    expect(isValid).toBeInTheDocument();
+  })
+})
