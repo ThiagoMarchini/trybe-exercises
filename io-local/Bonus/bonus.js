0bbc4f759a4afa9525f60c760a99c6354db4033d
Thiago Marchini <thiago@msn.com>
2021-08-25 17:33:32 -0300

    Exercícios bonus

--- io-local/Bonus/bonus.js
+++ io-local/Bonus/bonus.js
@@ -15,8 +15,18 @@ function questionUser(message) {
   });
 }
 
-async function main() {
-  const fileName = await questionUser('Digite o nome do arquivo para leitura: ');
+async function readFile() {
+  let fileName = await questionUser('Digite o nome do arquivo para leitura: ');
+  fileName = './Bonus/' + fileName;
+
+  fs.readFile(fileName, 'utf-8')
+    .then((fileContent) => console.log(fileContent))
+    .catch((err) => console.error(`Não foi possível abrir o arquivo: ${err.message}`));
+}
+
+async function changeWord() {
+  let fileName = await questionUser('Digite o nome do arquivo para leitura: ');
+  fileName = './Bonus/' + fileName;
 
   const originalContent = await fs.readFile(fileName, 'utf-8')
     .catch(err => {
@@ -24,6 +34,17 @@ async function main() {
       return false;
     });
   if (!originalContent) return;
+  
+  const wordToReplace = await questionUser('Digite a palavra a ser substituída: ');
+  const replacement = await questionUser('Digite a nova palavra: ');
+  const newContent = originalContent.replace(new RegExp(wordToReplace, 'g'), replacement);
+  console.log(`Novo conteúdo:\n${newContent}`);
+
+  let newFileName = await questionUser('Digite o nome para o novo arquivo: ');
+  newFileName = './Bonus/' + newFileName;
+  fs.writeFile(newFileName, newContent)
+    .then(() => console.log('Arquivo salvo com sucesso!'))
+    .catch((err) => console.error(`Não foi possível salvar o arquivo: ${err.message}`));
 }
 
-main();
+changeWord();
