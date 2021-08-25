b12af48fdda58b61b888aff058c442305144c3b8
Thiago Marchini <thiago@msn.com>
2021-08-25 06:28:19 -0300

    Commit inicial

--- hello-world/index.js
+++
@@ -1,9 +0,0 @@
-const readLine = require('readline-sync');
-
-// console.log('Hello, world!');
-
-const name = readLine.question('Qual é o seu nome?\n');
-
-const age =  readLine.questionInt('Qual é a sua idade?\n');
-
-console.log(`Hello, ${name}! You are ${age} years old.`);
